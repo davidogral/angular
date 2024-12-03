8edf5af2e36e6739277f8157cbf74367efdd8f38
davidogral <111357302+davidogral@users.noreply.github.com>
2024-12-03 10:00:56 -0300

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SobreComponent } from './sobre/sobre.component';
-import { ContatoComponent } from './contato/contato.component';
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'sobre', component: SobreComponent },
-  { path: 'contato', component: ContatoComponent },
-];
+export const routes: Routes = [];
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-
+/*
+ *  Protractor support is deprecated in Angular.
+ *  Protractor is used in this example for compatibility with Angular documentation tools.
+ */
+import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import {provideRouter} from '@angular/router';
+import routeConfig from './app/routes';
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-  ]
-}).catch(err => console.error(err));
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
